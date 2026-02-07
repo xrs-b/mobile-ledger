3f9da7ba045cbbab5f1c4cf9374684a22490709e
root <root@C202512030993676.local>
2026-02-07 16:45:44 +0000

    Fix: remove vite-plugin-style-import dependency

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import pxtorem from 'postcss-pxtorem'
-import styleImport from 'vite-plugin-style-import'
 
 // https://vite.dev/config/
 export default defineConfig({
